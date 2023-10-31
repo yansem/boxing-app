f70fc2f170b10f9e50483e7f5647545c6e6c7ff9
yansem <janchevsem@mail.ru>
2023-10-31 15:31:46 +0300

    dev in progress

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,11 @@
 <template>
     <header class="bg-black flex justify-end">
-        <div>
-            <button class="bg-yellow-400 py-2 px-4 rounded">Войти</button>
+        <div v-if="isAuth">
+            <button class="bg-yellow-400 py-2 px-4 rounded">Логин</button>
+            <button class="bg-yellow-400 py-2 px-4 rounded">Выйти</button>
+        </div>
+        <div v-else>
+            <button class="bg-yellow-400 py-2 px-4 rounded" @click="isShowLogin = true">Войти</button>
             <button class="bg-yellow-400 py-2 px-4 rounded" @click="isShowRegister = true">Регистрация</button>
         </div>
     </header>
@@ -169,33 +173,62 @@
         </table>
     </div>
 
-    <div v-if="isShowRegister" class="fixed inset-0 flex items-center justify-center">
+    <div v-if="isShowLogin" class="fixed inset-0 flex items-center justify-center">
         <div class="modal-bg absolute inset-0 bg-black opacity-50"></div>
         <div class="modal-content bg-white p-4 rounded-lg relative">
             <!-- Close button -->
             <button id="closeModal" class="absolute top-0 right-0 p-2 cursor-pointer text-gray-600 hover:text-gray-800">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414L11.414 10l2.293 2.293a1 1 0 01-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 01-1.414-1.414L8.586 10 6.293 7.707a1 1 0 010-1.414z"/>
+                    <path fill-rule="evenodd"
+                          d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414L11.414 10l2.293 2.293a1 1 0 01-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 01-1.414-1.414L8.586 10 6.293 7.707a1 1 0 010-1.414z"/>
                 </svg>
             </button>
             <!-- Modal content, including form -->
-            <h1 class="text-2xl font-semibold">Modal Form</h1>
-            <form id="modalForm" class="mt-4">
+            <h1 class="text-2xl font-semibold">Вход</h1>
+            <form class="mt-4">
                 <!-- Form inputs go here -->
                 <div class="mb-4">
                     <label for="login" class="block font-medium text-gray-700">Логин</label>
-                    <input v-model="registerForm.login" type="text" id="login" name="login" class="w-full p-2 border rounded-lg">
+                    <input v-model="loginForm.login" type="text" id="login" name="login"
+                           class="w-full p-2 border rounded-lg">
                 </div>
                 <div class="mb-4">
                     <label for="password" class="block font-medium text-gray-700">Пароль</label>
-                    <input v-model="registerForm.password" type="password" id="password" name="password" class="w-full p-2 border rounded-lg">
-                </div>
-                <div class="mb-4">
-                    <label for="confirm" class="block font-medium text-gray-700">Подтвердите пароль</label>
-                    <input v-model="registerForm.confirm" type="password" id="confirm" name="confirm" class="w-full p-2 border rounded-lg">
+                    <input v-model="loginForm.password" type="password" id="password" name="password"
+                           class="w-full p-2 border rounded-lg">
                 </div>
                 <!-- Add more form fields as needed -->
-                <button class="bg-yellow-400 py-2 px-4 rounded" @click="registration">Ок</button>
+                <button class="bg-yellow-400 py-2 px-4 rounded" @click="login">Ок</button>
+            </form>
+        </div>
+    </div>
+
+    <div v-if="isShowRegister" class="fixed inset-0 flex items-center justify-center">
+        <div class="modal-bg absolute inset-0 bg-black opacity-50"></div>
+        <div class="modal-content bg-white p-4 rounded-lg relative">
+            <!-- Close button -->
+            <button class="absolute top-0 right-0 p-2 cursor-pointer text-gray-600 hover:text-gray-800">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                          d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414L11.414 10l2.293 2.293a1 1 0 01-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 01-1.414-1.414L8.586 10 6.293 7.707a1 1 0 010-1.414z"/>
+                </svg>
+            </button>
+            <!-- Modal content, including form -->
+            <h1 class="text-2xl font-semibold">Регистрация</h1>
+            <form @submit.prevent="registration" class="mt-4">
+                <!-- Form inputs go here -->
+                <div class="mb-4">
+                    <label for="login" class="block font-medium text-gray-700">Логин</label>
+                    <input v-model="registerForm.login" type="text" id="login" name="login"
+                           class="w-full p-2 border rounded-lg">
+                </div>
+                <div class="mb-4">
+                    <label for="password" class="block font-medium text-gray-700">Пароль</label>
+                    <input v-model="registerForm.password" type="password" id="password" name="password"
+                           class="w-full p-2 border rounded-lg">
+                </div>
+                <!-- Add more form fields as needed -->
+                <button class="bg-yellow-400 py-2 px-4 rounded">Ок</button>
             </form>
         </div>
     </div>
@@ -207,11 +240,17 @@ import InputNumber from "./components/Form/InputNumber.vue";
 import InputCheckbox from "./components/Form/InputCheckbox.vue";
 import {ref, computed, reactive, onMounted} from "vue";
 
+const isAuth = ref(false);
+
 const isShowRegister = ref(false);
+const isShowLogin = ref(false);
 const registerForm = reactive({
     login: '',
-    password: '',
-    confirm: ''
+    password: ''
+});
+const loginForm = reactive({
+    login: '',
+    password: ''
 });
 const voices = ref([]);
 const voiceSelected = ref(null);
@@ -221,15 +260,15 @@ const speakText = async (punch) => {
     const utterance = new SpeechSynthesisUtterance(punch);
     utterance.voice = voiceSelected.value;
     utterance.rate = 3;
-    utterance.onerror = function(event) {
+    utterance.onerror = function (event) {
         console.error('Speech synthesis error:', event.error);
     };
-    await new Promise(function(resolve) {
+    await new Promise(function (resolve) {
         utterance.onend = resolve;
         synth.cancel();
         synth.speak(utterance);
     });
-} ;
+};
 
 const getVoices = () => {
     if ('speechSynthesis' in window) {
@@ -461,9 +500,27 @@ const removeRound = (index) => {
     rounds.value.splice(index, 1);
 }
 
-const registration = () => {
-    axios.post('/api/registration', registerForm)
-        .then()
-        .catch()
+const registration = async () => {
+    await axios.post('/register', registerForm)
+        .then(response => {
+            isShowRegister.value = false;
+            isAuth.value = true;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+const login = async () => {
+    await axios.get('/sanctum/csrf-cookie').then(async response => {
+        await axios.post('/login', loginForm)
+            .then(response => {
+                isShowLogin.value = false;
+                isAuth.value = true;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    });
 }
 </script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use FFMpeg\Format\Audio\Mp3;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [LoginController::class, 'login']);
 
 Route::get('/', function () {
     return view('app');
